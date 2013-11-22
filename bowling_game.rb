70b303b73813f8996d01dd860289ad4735823114
chrisbodhi <cboette@gmail.com>
2013-11-21 22:37:36 -0600

    Add test and logic for scoring strikes. Install necessary gems locally.

--- bowling_game.rb
+++ bowling_game.rb
@@ -20,23 +20,27 @@ class BowlingGame
 
 	# Returns as an Integer the total of all the rolls in the @rolls array.
 	def score
+		# Inject was previously used to sum the score.
 		# @rolls.inject(0) {|result, number| result+number}
 		total_score = 0
 		current_roll = 0
 
 		while current_roll < @rolls.size
-			roll 		= @rolls[current_roll]
-			next_roll 	= @rolls[current_roll+1]
+			roll = @rolls[current_roll]
+			next_roll = @rolls[current_roll + 1]
 
-			if roll + next_roll == 10
-				total_score += 10+@rolls[current_roll+2]
-			else 
+			if roll == 10
+				total_score += 10 + next_roll + @rolls[current_roll + 2]
+				current_roll += 1
+			elsif roll + next_roll == 10
+				total_score += 10 + @rolls[current_roll + 2]
+				current_roll += 2
+			else
 				total_score += roll + next_roll
+				current_roll += 2
 			end
-
-			current_roll += 2
 		end
-
+					
 		return total_score
 	end
 end
