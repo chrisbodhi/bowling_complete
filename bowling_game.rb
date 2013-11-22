779fe56a4daf9df5f92206a6dbf69fea4e1ddf36
chrisbodhi <cboette@gmail.com>
2013-11-22 16:45:45 -0600

    Refactor the scoring method to cut back on that code stank. Create six new methods along with comments.

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,44 +3,81 @@ class BowlingGame
 
 	def initialize
 		@rolls = []
+		@total_score = 0
+		@current_roll = 0
 	end
 
 	# Record a roll in the BowlingGame
-
+	#
 	# pins - An Integer of pins that are knocked down in each roll.
-
+	#
 	# Returns nothing
 	def roll(pins)
 		@rolls << pins
 	end
 
 
-	# Returns the total score of all the rolls. Expects to be run
-	# at the end of the game.
-
-	# Returns as an Integer the total of all the rolls in the @rolls array.
+	# Returns the integer score for the game, taken from @rolls. Runs
+	# after all rolls have been recorded.
 	def score
-		# Inject was previously used to sum the score.
-		# @rolls.inject(0) {|result, number| result+number}
-		total_score = 0
-		current_roll = 0
+		while @current_roll < @rolls.size
+			init_roll
 
-		while current_roll < @rolls.size
-			roll = @rolls[current_roll]
-			next_roll = @rolls[current_roll + 1]
-
-			if roll == 10
-				total_score += 10 + next_roll + @rolls[current_roll + 2]
-				current_roll += 1
-			elsif roll + next_roll == 10
-				total_score += 10 + @rolls[current_roll + 2]
-				current_roll += 2
+			if strike?
+				score_strike
+			elsif spare?
+				score_spare
 			else
-				total_score += roll + next_roll
-				current_roll += 2
+				score_standard
 			end
 		end
 					
-		return total_score
+		return @total_score
+	end
+
+	# Here begins the methods used to score the Game of the Dude.
+	private
+
+	# Initialize values for roll and next_roll.
+  #
+  # Returns nothing.
+	def init_roll 
+		@roll 			= @rolls[@current_roll]
+		@next_roll 	= @rolls[@current_roll + 1]
+	end
+
+	# Returns true if the current roll is a strike, false otherwise.
+	def strike?
+		@roll == 10
+	end
+
+  # Scores a strike frame, and adds it to the total score for the game.
+  #
+  # Returns nothing.
+	def score_strike
+		@total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+		@current_roll += 1
+	end
+
+	# Returns true if the current roll and the next roll comprise a 
+	# spare, false otherwise.
+	def spare?
+		@roll + @next_roll == 10
+	end
+
+  # Scores a spare [two frames], and adds it to the total game score.
+  #
+  # Returns nothing.
+	def score_spare
+		@total_score += 10 + @rolls[@current_roll + 2]
+		@current_roll += 2
+	end
+
+  # Scores a standard roll, and adds it to the total score for the game.
+  #
+  # Returns nothing.
+	def score_standard
+		@total_score += @roll + @next_roll
+		@current_roll += 2
 	end
 end
